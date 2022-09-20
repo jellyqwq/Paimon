8855f27624e234e2216aab17e28ebff30b49e0b5
GuoHaoLan <coopticks@hotmail.com>
2022-09-20 22:39:45 +0800

    Add Requests test file

---
+++ requests/request_test.go
@@ -0,0 +1,12 @@
+package requests
+
+import (
+	"testing"
+)
+
+func TestBronya(t *testing.T) {
+	res := Bronya("POST", "https://httpbin.org/post", nil, `{"test":"ok"}`)
+	t.Logf("ResCode: %d", res.StatusCode)
+	t.Log(res.Header)
+	t.Logf("Content: %d", string(res.Body))
+}
