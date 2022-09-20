5ccc07836b25fc2087ccb3243e68b0e47cabdd7e
jellycreator <577430840@qq.com>
2022-09-20 23:19:14 +0800

    add a bug

--- requests/request_test.go
+++ requests/request_test.go
@@ -1,11 +1,23 @@
 package requests
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestBronya(t *testing.T) {
-	res, _ := Bronya("POST", "https://httpbin.org/post", nil, `{"test":"ok"}`)
+	text := `moon hola`
+	data := fmt.Sprintf(`{
+		"context": {
+			"client": {
+				"clientName": "WEB",
+				"clientVersion": "2.20220617.00.00"
+			}
+		},
+		"query": "%v"
+	}`, text)
+	t.Logf("data: %v", data)
+	res, _ := Bronya("POST", "https://www.youtube.com/youtubei/v1/search", nil, data)
 	t.Logf("ResCode: %d", res.StatusCode)
 	t.Log(res.Header)
 	t.Logf("Content: %s", string(res.Body))
